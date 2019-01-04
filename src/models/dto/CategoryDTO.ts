aaac05c581397227ff04758f7a9ee66bc8ca943a
APPLE4869 <apple.4869.s@gmail.com>
2019-01-04 21:12:07 +0900

    category一覧の表示を変更

--- src/models/dto/CategoryDTO.ts
+++ src/models/dto/CategoryDTO.ts
@@ -2,11 +2,30 @@ export default class CategoryDTO {
   readonly id: string;
   readonly name: string;
   readonly imageUrl: string;
+  readonly totalSubcategoryCount: number;
+  readonly totalPhraseCount: number;
+  readonly totalCommentCount: number;
+  readonly totalLikeCount: number;
+  readonly totalFavoriteCount: number;
 
-  constructor({ id, name, imageUrl }: CategoryProperty) {
+  constructor({
+    id,
+    name,
+    imageUrl,
+    totalSubcategoryCount,
+    totalPhraseCount,
+    totalCommentCount,
+    totalLikeCount,
+    totalFavoriteCount
+  }: CategoryProperty) {
     this.id = id;
     this.name = name;
     this.imageUrl = imageUrl;
+    this.totalSubcategoryCount = totalSubcategoryCount;
+    this.totalPhraseCount = totalPhraseCount;
+    this.totalCommentCount = totalCommentCount;
+    this.totalLikeCount = totalLikeCount;
+    this.totalFavoriteCount = totalFavoriteCount;
   }
 }
 
@@ -14,6 +33,11 @@ interface CategoryProperty {
   id: string;
   name: string;
   imageUrl: string;
+  totalSubcategoryCount: number;
+  totalPhraseCount: number;
+  totalCommentCount: number;
+  totalLikeCount: number;
+  totalFavoriteCount: number;
 }
 
 export interface CategoryResponse {
